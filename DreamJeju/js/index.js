ada68f60f7560bf40d0a1167e74aa3d344cff40e
wjdals1217 <wjdals1217@gmail.com>
2023-08-03 17:31:17 +0900

    dreamjeju

---
+++ DreamJeju/js/index.js
@@ -0,0 +1,49 @@
+$(function(){
+    const slides = $('.slides');
+    let i = 0;
+
+    $('.left').click(function(){
+        
+        slides.eq(i).animate({'left':'-100%'}, 1000);
+        i++;
+        if(i == 3) {
+            i=0;
+        }
+        slides.eq(i).css('left', '100%').animate({'left':'0'}, 1000);
+        
+    });
+    
+    $('.right').click(function(){
+        
+        slides.eq(i).animate({'left':'100%'}, 1000);
+        i++;
+        if(i == 3) {
+            i=0;
+        }
+        slides.eq(i).css('left', '-100%').animate({'left':'0'}, 1000);
+        
+    });
+    $('.right').click(function(){});
+
+});
+
+window.onload = function(){
+
+    const tabBtn = document.querySelectorAll('#tabMenu > label');
+    const tabContent = document.querySelectorAll('#tabMenu > section > article');
+
+    tabBtn[0].onclick = function(){
+        tabBtn[1].classList.remove('on');
+        this.classList.add('on');
+        tabContent[0].classList.add('on');
+        tabContent[1].classList.remove('on');
+    };
+    
+    tabBtn[1].onclick = function(){
+        tabBtn[0].classList.remove('on');
+        this.classList.add('on');
+        tabContent[1].classList.add('on');
+        tabContent[0].classList.remove('on');
+
+    };
+};

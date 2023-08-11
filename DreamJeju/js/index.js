d0bb44048e4ee931cea1d548785eb961b3a771b1
wjdals1217 <wjdals1217@gmail.com>
2023-08-11 17:52:42 +0900

    드림제주

--- DreamJeju/js/index.js
+++ DreamJeju/js/index.js
@@ -1,9 +1,15 @@
+// 슬라이드 배너
+    let slideSection = $('#slideShow > section');
+    let slides = $('.slides');
+    slideCount = slides.length;
+    currentIdx = 0;
+    makeClone(); // 처음 이미지와 마지막 이미지 복사 함수
+    initfunction(); // 슬라이드 넓이와 위치값 초기화 함수
+
 $(function(){
-    const slides = $('.slides');
-    let i = 0;
-   
+       
     $('.left').click(function(){
-        
+        slideWidth = 1200; // 슬라이드 이미지 넓이
         slides.eq(i).animate({'left':'-100%'}, 1000);
         i++;
         if(i == 3) {
@@ -26,6 +32,16 @@ $(function(){
 
 
 });
+function makeClone(){
+    let cloneSlide_firtst = slides[0].clonNode(true);
+    let cloneSlide_last = slidesSection.lastElementChild.cloneNode(true);
+    slidesSection.append(cloneSlide_firtst);
+    slideSection.insertBefore(cloneSlide_last, slideSection.firstElementChild);
+}
+function initfunction(){
+    slideSection.style.width = slideWidth * (slideCount + 2) + 'px';
+    slideSection.style.left = -slideWidth + 'px';
+}
 
 window.onload = function(){
 
